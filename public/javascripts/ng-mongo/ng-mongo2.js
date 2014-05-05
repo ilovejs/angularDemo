fee70ea12ec2ba6e78e9cd9ba332c37885358468
michaelzhuang <m.zhuang@arc.unsw.edu.au>
2014-05-05 13:40:21 +1000

    ep2 - after lesson 5 db create / delete

--- public/javascripts/ng-mongo/ng-mongo2.js
+++ public/javascripts/ng-mongo/ng-mongo2.js
@@ -14,7 +14,22 @@ ngMongo.factory("Mongo", function($resource){
 
 ngMongo.controller("ListCtrl", function($scope, Mongo){
    $scope.items = Mongo.database.query({}, isArray = true);
-    DB = $scope.items;
+
+   $scope.addDb = function(){
+       var dbName = $scope.newDbName;
+       if(dbName){
+           var newDb = new Mongo.database({name: dbName});
+           newDb.$save(); //use http POST
+           $scope.items.push(newDb); //check node console info to make sure it is POST
+       }
+   };
+
+   $scope.removeDb = function(item){
+        if(confirm("Remove database?")){
+            item.$delete({name: item.name});
+            $scope.items.splice($scope.items.indexOf(item),1);
+        }
+   };
 });
 
 //mongo-api/dbs
